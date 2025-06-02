b6e99d281756200c424302b5c87556dea9857839
akshay <akshaykomade012345@gmail.com>
2025-06-02 17:19:45 +0530

    samesite -> None

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -32,7 +32,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
       .cookie("accessToken", newAccessToken, {
         httpOnly: true,
         secure: false,
-        sameSite: "Lax",
+        sameSite: "None",
         maxAge: 15 * 60 * 1000,
       })
       .json({ token: newAccessToken });
@@ -165,13 +165,13 @@ const login = asyncHandler(async (req, res) => {
     .cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: false,
-      sameSite: "Lax",
+      sameSite: "None",
       maxAge: 15 * 60 * 1000, // 15 minutes
     })
     .cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: false,
-      sameSite: "Lax",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
     .status(200)
